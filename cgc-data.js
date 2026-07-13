a5644c8e25a5235da83e356ca79c29177c85d954
Rocco1982 <cicciarella@gmail.com>
2026-07-13 14:10:42 -0700

    update CGC data

--- cgc-data.js
+++ cgc-data.js
@@ -1,5 +1,5 @@
 window.CGC_DATA = {
-  generatedAt: "2026-07-13T14:09:40-07:00",
+  generatedAt: "2026-07-13T14:10:42-07:00",
   source: "Gift Card Book and Balances / Main",
   sort: "Created At descending (active marketplace cards)",
   expiryDays: 14,
