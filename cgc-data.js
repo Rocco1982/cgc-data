95049f914e02818722c3b17768b9ea3751356baa
Rocco1982 <cicciarella@gmail.com>
2026-08-18 07:00:09 -0700

    update CGC data

--- cgc-data.js
+++ cgc-data.js
@@ -1,9 +1,22 @@
 window.CGC_DATA = {
-  generatedAt: "2026-08-18T06:00:04-07:00",
+  generatedAt: "2026-08-18T07:00:08-07:00",
   source: "Gift Card Book and Balances / Main",
   sort: "Created At descending (active marketplace cards)",
   expiryDays: 14,
   deals: [
+  {
+    "sheetRow": 380,
+    "createdAt": "2026-08-18T07:00:00.000Z",
+    "brand": "LCBO",
+    "ref": "GCFC-L95050",
+    "faceValue": "$100.00",
+    "saleValue": "$85.00",
+    "discount": "15.00%",
+    "image": "https://assets.cdn.filesafe.space/FxfSu0nbVobswu1XWMTK/media/69f01235fa0a66d4caa86e13.jfif",
+    "marketStatus": "buyer_interest",
+    "listedAt": "",
+    "expiresAt": ""
+  },
   {
     "sheetRow": 371,
     "createdAt": "2026-08-17T07:00:00.000Z",
