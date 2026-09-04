700bbbbaeb71e62f0de59ad231cba27719eb903c
Rocco1982 <cicciarella@gmail.com>
2026-09-04 15:11:13 -0700

    update CGC data

--- cgc-data.js
+++ cgc-data.js
@@ -1,5 +1,5 @@
 window.CGC_DATA = {
-  generatedAt: "2026-09-04T14:11:14-07:00",
+  generatedAt: "2026-09-04T15:11:13-07:00",
   source: "Gift Card Book and Balances / Main",
   sort: "Created At descending (active marketplace cards)",
   expiryDays: 14,
@@ -101,8 +101,8 @@ window.CGC_DATA = {
     "brand": "Apple Store",
     "ref": "GCFC-X43269",
     "faceValue": "$100.00",
-    "saleValue": "$85.00",
-    "discount": "15.00%",
+    "saleValue": "$80.00",
+    "discount": "20.00%",
     "image": "https://assets.cdn.filesafe.space/FxfSu0nbVobswu1XWMTK/media/69f24e48663e5f92fff67b45.png",
     "marketStatus": "buyer_interest",
     "listedAt": "2026-09-03 00:00:00",
