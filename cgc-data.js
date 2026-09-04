8907160329d991893a6c159e8818d88461c2a06a
Rocco1982 <cicciarella@gmail.com>
2026-09-04 02:16:39 -0700

    update CGC data

--- cgc-data.js
+++ cgc-data.js
@@ -1,5 +1,5 @@
 window.CGC_DATA = {
-  generatedAt: "2026-09-04T01:16:39-07:00",
+  generatedAt: "2026-09-04T02:16:39-07:00",
   source: "Gift Card Book and Balances / Main",
   sort: "Created At descending (active marketplace cards)",
   expiryDays: 14,
@@ -14,7 +14,7 @@ window.CGC_DATA = {
     "discount": "15.00%",
     "image": "https://assets.cdn.filesafe.space/FxfSu0nbVobswu1XWMTK/media/69f2581bfc17decc45fa0373.png",
     "marketStatus": "buyer_interest",
-    "listedAt": "",
+    "listedAt": "2026-08-27 16:44:51",
     "expiresAt": ""
   },
   {
